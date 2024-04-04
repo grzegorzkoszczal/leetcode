d2a858f724e31cd4146f17962460f4cd9fc31b67
Grzegorz  Koszczał <s175405@student.pg.edu.pl>
2024-04-04 14:10:01 +0200

    update

--- problems.py
+++ problems.py
@@ -834,3 +834,42 @@ class Solution:
                     root.right = TreeNode(val)
                     return old_root
         return TreeNode(val)
+
+
+"""
+450. Delete Node in a BST
+Topics: Tree, Binary Search Tree, Binary Tree
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def find_min(self, root):
+        while root and root.left:
+            root = root.left
+        return root
+
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if not root:
+            return None
+
+        if key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        elif key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        else:
+            # target node has 0 or 1 child
+            if not root.left:
+                return root.right
+            elif not root.right:
+                return root.left
+            else:
+                leaf = self.find_min(root.right)
+                root.val = leaf.val
+                root.right = self.deleteNode(root.right, leaf.val)
+        return root
