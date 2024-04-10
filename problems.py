4590dfe02acda4d0758e78e8265d19638086ffc3
Grzegorz  Koszczał <s175405@student.pg.edu.pl>
2024-04-10 20:00:30 +0200

    update

--- problems.py
+++ problems.py
@@ -1,4 +1,6 @@
 import math
+import heapq
+from heapq import *
 from collections import deque, defaultdict
 from typing import Optional, TypeAlias
 
@@ -1470,3 +1472,30 @@ class Solution:
                 indices.append(indices.popleft())
 
         return ans
+
+"""
+703. Kth Largest Element in a Stream
+Topics: Tree, Design, Binary Search Tree, Heap (Priority Queue), Binary Tree, Data Stream
+"""
+class KthLargest:
+
+    def __init__(self, k: int, nums: list[int]):
+        self.nums = nums[:k]
+        self.k = k
+        heapify(self.nums)
+        for i in range(k, len(nums)):
+            if nums[i] > self.nums[0]:
+                heappushpop(self.nums, nums[i])
+
+    def add(self, val: int) -> int:
+        if len(self.nums) < self.k:
+            heappush(self.nums, val)
+        elif val > self.nums[0]:
+            heapreplace(self.nums, val)
+
+        return self.nums[0]
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
